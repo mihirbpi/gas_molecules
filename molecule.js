c422a2246a43fc7462d8b8b401b5e36d3c5046e7
mihirbpi <mihir.borkar@gmail.com>
2019-08-17 16:09:17 -0700

    -Added gas_molecules code

---
+++ molecule.js
@@ -0,0 +1,55 @@
+class Molecule {
+
+  constructor(mass, temperature) {
+    /* Constructor to create a new molecule */
+    this.mass = mass;
+    this.temperature = temperature;
+    this.position = createVector(random(0, width), random(0, height));
+    this.radius = this.mass / 5;
+    this.velocity = createVector(sqrt(this.temperature / this.mass) * randomGaussian(0, 1), sqrt(this.temperature / this.mass) * randomGaussian(0, 1));
+  }
+
+  update() {
+    /* Update the molecule */
+
+    //Update the position of the molecule
+    this.position.add(this.velocity);
+
+    //If the mass slider is changed change the mass of the molecule
+    if (this.mass != mass_slider.value()) {
+      this.mass = mass_slider.value();
+      mass_text.html("Mass: " + mass_slider.value());
+      this.radius = this.mass / 5;
+    }
+
+    //If the temperature slider is changed change the tempareture & velocity of the molecule according to the Maxwell-Boltzmann distribution
+    if (this.temperature != temp_slider.value()) {
+      this.temperature = temp_slider.value();
+      temp_text.html("Temperature: " + temp_slider.value());
+      this.velocity = createVector(sqrt(this.temperature / this.mass) * randomGaussian(0, 1), sqrt(this.temperature / this.mass) * randomGaussian(0, 1));
+    }
+
+    //If the molecule hits the edge of the canvas make it bounce backwards with the same velocity
+    if (this.position.x <= 0 || this.position.y <= 0 || this.position.x >= window.width || this.position.y >= window.height) {
+      this.velocity = p5.Vector.mult(this.velocity, -1);
+    }
+
+    //If another molecule collides with this one make them elastically bounce off of each other
+    for (let other_molecule of molecules) {
+      var d = dist(this.position.x, this.position.y, other_molecule.position.x, other_molecule.position.y);
+      if (d <= this.radius) {
+        var v1i = this.velocity;
+        var v2i = other_molecule.velocity;
+        this.velocity = v2i;
+        other_molecule.velocity = v1i;
+      }
+    }
+  }
+
+  show() {
+    /* Display the molecule */
+    stroke(255);
+    fill(255)
+    circle(this.position.x, this.position.y, this.radius);
+  }
+}
---
+++ sketch.js
@@ -0,0 +1,37 @@
+const molecules = [];
+
+
+function setup() {
+  /* Setup the animation */
+
+  //Create sliders to change mass and temperature
+  createElement('p', 'Slide to change mass');
+  mass_slider = createSlider(1, 1000, 50);
+  mass_text = createElement('p', "Mass: " + mass_slider.value());
+  createElement('p', 'Slide to change temperature');
+  temp_slider = createSlider(0, 10000, 273);
+  temp_text = createElement('p', "Temperature: " + temp_slider.value());
+
+  //Create canvas
+  createCanvas(window.outerWidth, window.outerHeight);
+
+  //Create 200 molecules
+  for (var i = 0; i < 200; i++) {
+    molecule = new Molecule(mass_slider.value(), temp_slider.value());
+    molecules.push(molecule);
+  }
+
+}
+
+function draw() {
+  /* The draw loop which is called continuously */
+
+  //Create a dark gray background
+  background(51);
+
+  //Update and show all the molecules
+  for (let molecule of molecules) {
+    molecule.update();
+    molecule.show();
+  }
+}
